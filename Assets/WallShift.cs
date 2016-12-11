4717c660abf8428d8fcff52517454abaff71c7da
Alek Fleischer <thecrazyal02@gmail.com>
2016-12-11 15:40:10 -0800

    Remove debug logs

--- Assets/WallShift.cs
+++ Assets/WallShift.cs
@@ -8,28 +8,23 @@ public class WallShift : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("HMM");
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if ((UnityStandardAssets.Characters.FirstPerson.FirstPersonController.freeze) && (triggered == false)) {
-			Debug.Log("YO");
 			triggered = true;
 			StartCoroutine(Shift());
 		}
 		if (shifting) {
-			Debug.Log("HOOOOO!");
 			float p = transform.position.z;
 			p += Time.deltaTime * 5;
 			transform.position = new Vector3 (transform.position.x,transform.position.y,p);
 		}
 	}
 	IEnumerator Shift () {
-		Debug.Log("HO");
 		yield return new WaitForSeconds(20f);
 		shifting = true;
-		Debug.Log("BADOH");
 		yield return null;
 	}
 }
